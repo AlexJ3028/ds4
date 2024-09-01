160b78f414588685c268dfa665896aef9c8c3846
ALEX JIMENEZ <alex.jimenez@utp.ac.pa>
2024-08-31 21:09:26 -0500

    laboratorio 2 completo.

---
+++ Laboratorio 2/Laboratorio 2/Lab2.1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Laboratorio21
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Asignando valor a la variable estática.
+            MyClass.valor = 1;
+            Console.WriteLine(MyClass.valor);
+        }
+    }
+
+    public class MyClass
+    {
+        // Declarando variables estáticas.
+        public static int valor;
+    }
+}
---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Client client = new Client();
+            // Variables de instancia de clase (ejemplos).
+            client.FirstName = "su_nombre";
+            client.LastName = "su_apellido";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+    public class Client
+    {
+        // Declaración de variables de instancia en clases.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
+
