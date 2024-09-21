b40b7be6130b9cf3ee63d5032210d03c74654aec
ALEX JIMENEZ <alex.jimenez@utp.ac.pa>
2024-09-20 22:30:12 -0500

    Lab 7 completado

---
+++ Lab 7.1/Lab 7.1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Lab71
+{
+    class Cliente
+    {
+        private string nombre;
+        private int monto;
+
+        public Cliente(string nom)
+        {
+            nombre = nom;
+            monto = 0;
+        }
+
+        public void Depositar(int m)
+        {
+            monto += m;
+        }
+
+        public void Extraer(int m)
+        {
+            monto -= m; // Correcto: resta el monto
+        }
+
+        public int RetornarMonto() // Cambiado a int para que devuelva el valor del monto
+        {
+            return monto;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine(nombre + " tiene depositado la suma de " + monto);
+        }
+    }
+
+    class Banco
+    {
+        private Cliente cliente1, cliente2, cliente3;
+
+        public Banco()
+        {
+            cliente1 = new Cliente("Yhonas");
+            cliente2 = new Cliente("Ana");
+            cliente3 = new Cliente("Pedro");
+        }
+
+        public void Operar()
+        {
+            cliente1.Depositar(100);
+            cliente2.Depositar(150);
+            cliente3.Depositar(200); // Corrección del nombre de método
+            cliente3.Extraer(150);
+        }
+
+        public void DepositosTotales()
+        {
+            int t = cliente1.RetornarMonto() +
+                    cliente2.RetornarMonto() +
+                    cliente3.RetornarMonto();
+            Console.WriteLine("El total del dinero en el banco es: " + t);
+            cliente1.Imprimir();
+            cliente2.Imprimir();
+            cliente3.Imprimir();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 7.1/Lab 7.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lab72
+{
+    class Dado
+    {
+        private Random aleatorio;
+        private int valor;
+
+        public Dado()
+        {
+            aleatorio = new Random();
+        }
+
+        public void Tirar()
+        {
+            valor = aleatorio.Next(1, 7);
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("El valor del dado es: " + valor);
+        }
+
+        public int RetornarValor()
+        {
+            return valor;
+        }
+    }
+
+    class JuegoDeDados
+    {
+        private Dado dado1, dado2, dado3;
+
+        public JuegoDeDados()
+        {
+            dado1 = new Dado();
+            dado2 = new Dado();
+            dado3 = new Dado();
+        }
+
+        public void Jugar()
+        {
+            dado1.Tirar();
+            dado1.Imprimir();
+            dado2.Tirar();
+            dado2.Imprimir();
+            dado3.Tirar();
+            dado3.Imprimir();
+
+            if (dado1.RetornarValor() == dado2.RetornarValor() && dado1.RetornarValor() == dado3.RetornarValor
