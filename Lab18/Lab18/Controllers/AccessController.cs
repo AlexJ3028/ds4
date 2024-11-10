25b638be6152018ad0fea8aa0a8f963d5adf48dc
ALEX JIMENEZ <alex.jimenez@utp.ac.pa>
2024-11-10 00:22:34 -0500

    Lab 18 terminado.

---
+++ Lab18/Lab18/Controllers/AccessController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab18.Controllers
+{
+    public class AccessController : Controller
+    {
+// GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("ocurrio un error:(" + ex.message);
+            }
+        }
+    }
+        
+    
+}
---
+++ Lab18/Lab18/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab18.MvcApplication" Language="C#" %>
